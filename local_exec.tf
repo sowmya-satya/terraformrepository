dc7a5f6f05128dad7ff4ae9ec4b379b7004a6102
sowmya-satya <58682121+sowmya-satya@users.noreply.github.com>
2020-02-16 11:31:19 +0530

    Update local_exec.tf

--- local_exec.tf
+++ local_exec.tf
@@ -6,12 +6,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-west-1"
   
 }
 
 resource "aws_instance" "backend" {
-  ami                    = "ami-0c28d7c6dd94fb3a7"
+  ami                    = "ami-094f0176b0d009d9f"
   instance_type          = "t2.micro"
   key_name               = "${var.key_name}"
   vpc_security_group_ids = ["${var.sg-id}"]
